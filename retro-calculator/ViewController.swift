09d33a8538258684ac92022d5c98875ff7a72bf6
lucas fernández <6lucyfer9@gmail.com>
2016-03-17 14:10:20 +0100

    "First Version"

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -7,12 +7,119 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    //var
+    var btnSound: AVAudioPlayer!
+    var runningNumber: String = ""
+    var leftValStr: String = ""
+    var rightValStr: String = ""
+    var result: String = ""
+    var currentOperation: Operation = Operation.Empty
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    
+    //Outlets
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    
+    //Actions
+    @IBAction func numberPressed(btn: UIButton){
+        playSound()
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    @IBAction func onDividePress(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+    
+    @IBAction func onMultiplyPress(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onSubtactPressed(sender: AnyObject) {
+        processOperation(Operation.Substract)
+    }
 
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        playSound()
+        leftValStr = ""
+        rightValStr = ""
+        result = ""
+        runningNumber = ""
+        currentOperation = Operation.Empty
+        outputLbl.text = "0"
+    }
+    
+    
+    //functions
+    func processOperation(op: Operation){
+        playSound()
+        
+        if currentOperation != Operation.Empty{
+            
+            if runningNumber != ""{
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply{
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                }else if currentOperation == Operation.Divide{
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                }else if currentOperation == Operation.Substract{
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                }else if currentOperation == Operation.Add{
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                leftValStr = result
+                outputLbl.text = result
+            }
+            
+            currentOperation = op
+            
+        } else{
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+        }
+    }
+    
+    func playSound(){
+        if btnSound.play(){
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let url = NSURL(fileURLWithPath: path!)
+        
+        do{
+        try btnSound = AVAudioPlayer(contentsOfURL: url)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
